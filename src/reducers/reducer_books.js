660a8818b5453f3cd44807d4ce979cfa2cd3dee6
Gabriel Lu <gabriellu@GLu-MacBook-Pro.singnet.com.sg>
2018-02-01 18:00:31 +0800

    completed tutorial part 2

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'How to make friends and influence people', pages: 300 },
+    { title: 'Harry Potter and the Chamber of Secrets', pages: 20 },
+    { title: 'Fahrenheit 451', pages: 999 },
+    { title: 'Tintin', pages: 34 }
+  ]
+}
